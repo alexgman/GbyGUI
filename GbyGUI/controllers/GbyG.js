3fd75526f876434e7ac2d617d933de6d5412dccf
Alex Gordon <aleksandr.gordon@gmail.com>
2016-06-28 15:25:34 -0500

    Consuming GbyG API.

---
+++ GbyGUI/controllers/GbyG.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+var app = angular.module('myApp', []);
+app.controller('GetAllSamplesByStatusUser',
+  function ($scope, $http) {
+      $http.get('http://localhost:36059/api/Samples/GetAllSamplesByStatusUser')
+        .then(function (data) {
+            $scope.dataset = data.data;
+        });
+  }
+);
